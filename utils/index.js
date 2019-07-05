df4be79a8ae660a5c9435a98c641fbd992508939
Timothy Waldron <tim@waldron.im>
2019-07-05 18:36:44 +1000

    Adjusted draw logic

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,6 @@
 class Utils {
   static draw(dataSet) {
-    if (typeof(dataSet) === String)
-      return dataSet
-    else
-      return dataSet[Math.floor(Math.random() * dataSet.length)];
+    return dataSet[Math.floor(Math.random() * dataSet.length)];
   }
 }
 

9deed3d192d77f475d502618bf7aa4fbb8be24c2
Timothy Waldron <tim@waldron.im>
2019-07-09 07:57:32 +1000

    Implemented randomizer for '#' characters, Address library complete

--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,19 @@ class Utils {
   static draw(dataSet) {
     return dataSet[Math.floor(Math.random() * dataSet.length)];
   }
+  static randomizer(param) {
+    let charArray = param.split('');
+    let result = "";
+
+    charArray.forEach((char) => {
+      if (char === "#")
+        result += Math.floor(Math.random() * 10).toString();
+      else
+        result += char
+    });
+
+    return result
+  }
 }
 
 module.exports = Utils;
